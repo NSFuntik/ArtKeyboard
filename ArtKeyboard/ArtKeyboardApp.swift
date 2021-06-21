c1b81f8d4b4dd2597730e4b63bb483f97c1eb67e
Gunter <w>
2021-06-21 13:49:33 +0100

    Initial Commit

---
+++ ArtKeyboard/ArtKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtKeyboardApp.swift
+//  ArtKeyboard
+//
+//  Created by Günter on 21.06.21.
+//
+
+import SwiftUI
+
+@main
+struct ArtKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
